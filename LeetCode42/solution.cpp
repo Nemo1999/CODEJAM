3fadf715b5bea38a47cc0ddeb5f0fcb55732a9bc
nemo <nemo1999.eecs06@g2.nctu.edu.tw>
2023-02-08 12:20:50 +0800

    Fix bug

--- LeetCode42/solution.cpp
+++ LeetCode42/solution.cpp
@@ -25,6 +25,24 @@ public:
             }
             st.push(pair<int, int>(*it, it-height.begin()));
         }
+        while(!st.empty()){st.pop();}
+        for(auto it=height.end()-1; it>=height.begin(); it--){
+            while(!st.empty()){
+                pair<int, int> t = st.top();
+                if(t.first <= *it){
+                    st.pop();
+                    int new_right = t.second;
+                    if(puddle_right_edge.count(it-height.begin()))
+                        new_right =  max(t.second, puddle_right_edge[it-height.begin()]);
+                    puddle_right_edge[it-height.begin()] = new_right;
+                }
+                else{
+                    break;
+                }
+            }
+            st.push(pair<int, int>(*it, it-height.begin()));
+        }
+
         ans = 0;
         vector<int>::iterator current_left = height.end();
         vector<int>::iterator current_right = height.end();
@@ -36,7 +54,7 @@ public:
                 }
             }
             else if(it < current_right){
-                ans += *current_left - *it;
+                ans += min( *current_left, *current_right) - *it;
             }
             else{ //it = current_right
                 if(puddle_right_edge.count(it-height.begin())){
@@ -54,8 +72,8 @@ public:
 
 int main(){
     Solution sol;
-    //vector<int> heights = { 0,1,0,2,1,0,1,3,2,1,2,1};
-    vector<int> heights = { 3,2,1,1,2,4,0,4};
+    vector<int> heights = { 0,1,0,2,1,0,1,3,2,1,2,1};
+    //vector<int> heights = { 5,0,4};
     cout << sol.trap(heights)<< endl;
     return 0;
 }
